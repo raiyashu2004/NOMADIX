2fda4f32eb053a5242d0298d4120c84389212ff5
Shikhar Shaurya <shikharshauryarajput@gmail.com>
2026-02-26 21:04:21 +0530

    feat: initial frontend setup with Vite, Tailwind, ESLint and Husky

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-900'>
+        Hello :git statusD
+      </div>
+    </>
+  )
+}
+
+export default App
